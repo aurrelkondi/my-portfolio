128c2c794c355883b758997de68fc6387fdc5afc
aurrelkondi <aurrelkondi@hotmail.com>
2022-06-20 14:58:04 +0200

    Done

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,6 +1,77 @@
 import React from "react";
 import "./Experience";
+import { BsFillBookmarkCheckFill } from "react-icons/bs";
+import "./experience.css";
 
 export default function Experience() {
-  return <section id="experience">Experience</section>;
+  return (
+    <section id="experience">
+      <h5>What skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillBookmarkCheckFill />
+              <h4>HTML</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsFillBookmarkCheckFill />
+              <h4>CSS</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsFillBookmarkCheckFill />
+              <h4>JS</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsFillBookmarkCheckFill />
+              <h4>BOOTSTRAP</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsFillBookmarkCheckFill />
+              <h4>REACT</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+          </div>
+        </div>
+        {/* END OF FRONTEND */}
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillBookmarkCheckFill />
+              <h4>Node JS</h4>
+              <small className="text-light">Basic</small>
+            </article>
+            <article className="experience__details">
+              <BsFillBookmarkCheckFill />
+              <h4>Mongo DB</h4>
+              <small className="text-light">Basic</small>
+            </article>
+            <article className="experience__details">
+              <BsFillBookmarkCheckFill />
+              <h4>PHP</h4>
+              <small className="text-light">Basic</small>
+            </article>
+            <article className="experience__details">
+              <BsFillBookmarkCheckFill />
+              <h4>MySQL</h4>
+              <small className="text-light">Basic</small>
+            </article>
+            <article className="experience__details">
+              <BsFillBookmarkCheckFill />
+              <h4>PYTHON</h4>
+              <small className="text-light">Basic</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,6 +1,99 @@
 import React from "react";
 import "./services.scss";
+import { FaCheckCircle } from "react-icons/fa";
 
 export default function Services() {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <FaCheckCircle className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <FaCheckCircle className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <FaCheckCircle className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <FaCheckCircle className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <FaCheckCircle className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF UI/UX */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <FaCheckCircle className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <FaCheckCircle className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <FaCheckCircle className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <FaCheckCircle className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <FaCheckCircle className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF WEB DEVELOPMENT */}
+        <article className="service">
+          <div className="service__head">
+            <h3>CONTENT CREATIONS</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <FaCheckCircle className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <FaCheckCircle className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <FaCheckCircle className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <FaCheckCircle className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <FaCheckCircle className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF CONTENT CREATIONS */}
+      </div>
+    </section>
+  );
 }
