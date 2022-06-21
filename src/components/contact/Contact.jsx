eb1b0c3f7d172cc757379da5b41448dbfe0e00cb
aurrelkondi <aurrelkondi@hotmail.com>
2022-06-21 12:00:36 +0200

    Done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,68 @@
 import React from "react";
 import "./Contact";
+import "./contact.scss";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 
 export default function Contact() {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>aurrelkondi@hotmail.com</h5>
+            <a href="mailto:aurrelkondi@hotmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className="contact__option-icon" />
+            <h4>Messenger</h4>
+            <h5>rrelkondi</h5>
+            <a href="https://m.me/rrelkondi1" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>+49999999999</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone+49999999999"
+              target="_blank"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+
+          <input type="email" name="email" placeholder="Your Email" required />
+
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,72 @@
 import React from "react";
 import "./testimonials.scss";
+import AVTR1 from "../../assets/AVRT-1.jpg";
+import AVTR2 from "../../assets/AVRT-2.jpg";
+import AVTR3 from "../../assets/AVRT-3.jpg";
+import AVTR4 from "../../assets/AVRT-4.jpg";
+
+// import Swiper core and required modules
+import { Pagination } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: "Tina",
+    review:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ipsum!",
+  },
+  {
+    avatar: AVTR2,
+    name: "Tina",
+    review:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ipsum!",
+  },
+  {
+    avatar: AVTR3,
+    name: "Tina",
+    review:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ipsum!",
+  },
+  {
+    avatar: AVTR4,
+    name: "Tina",
+    review:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ipsum!",
+  },
+];
 
 export default function Testimonials() {
-  return <section id="testimonials">Testimonials</section>;
+  return (
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper
+        className="container testimonials__container"
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial">
+              <div className="clients__avatar">
+                <img src={avatar} alt="Avatar one" />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
 }
